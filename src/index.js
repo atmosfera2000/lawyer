fe8cbfee7b64e63948516d854e6f6f16e2c4f4b5
atmosfera2000 <atmosfera2000@mail.ru>
2024-03-11 15:36:02 +0300

    css animation fix

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ const MOBILE_SLIDER = document.getElementById('mobileSlider');
 const SLIDER_BREAKPOINT = '(min-width: 768px)';
 const ELEMENTS_EFFECT = document.querySelectorAll('.animate__animated.hidden')
 
+if (window.scrollY) {
+  Array.from(document.querySelectorAll('.hidden')).forEach( element => element.classList.remove("hidden") )
+}
+
 window.addEventListener('DOMContentLoaded', function() {  
   navbar();
   mobileSlider(MOBILE_SLIDER, SLIDER_BREAKPOINT);
   slider();
-  scrollEffect(ELEMENTS_EFFECT, 'animate__fadeInUp')
+  if (!window.scrollY) {
+    scrollEffect(ELEMENTS_EFFECT, 'animate__fadeInUp')
+  }  
 })
 
 
